b19b0bc23a04ee8a3c18ad6b811744192354edbd
Triliam <taisbuenovidotto@gmail.com>
2023-06-16 15:32:55 -0300

    refatoracao

--- listas/exer71.py
+++ listas/exer71.py
@@ -2,7 +2,7 @@ primeiroTermo = 5
 razao = 2
 qtd = 5
 
-for i in range (qtd+1):
+for i in range (0, qtd):
     print(primeiroTermo)
     primeiroTermo = primeiroTermo * 2 #usando a mesma logica  do exercicio 64, mas agora numa PG
 

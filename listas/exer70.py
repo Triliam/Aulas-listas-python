b44dbbc909ea4df26f6822194fa1ca646d8f8328
Triliam <taisbuenovidotto@gmail.com>
2023-06-16 15:25:12 -0300

    refatoracao

--- listas/exer70.py
+++ listas/exer70.py
@@ -1,4 +1,7 @@
 #exibir os numeros entre 5 e 15
 
-for i in range(6, 15):
+num1 = int(input("primeiro numero: "))
+num2 = int(input("segundo numero: "))
+
+for i in range(num1 +1, num2):
     print(i)
